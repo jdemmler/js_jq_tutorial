3fcd544a9d5a6fe32382b6417aa5acc375ef4f0c
jdemmler <joannedemmler@gmail.com>
2014-07-10 19:30:45 -0700

    set up

---
+++ index.php
@@ -0,0 +1,328 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>JS & JQ | Continuous Learning</title>
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" type="text/css" href="styles/styles.css">
+<!--[if lt IE 9]>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+
+<body id="back_top">
+
+<header>
+<div class="inner-header">
+
+<div class="logo">
+<h1>JavaScript & jQuery</h1>
+
+<div id="infinity-logo"></div>
+
+<h2>Continuous Learning</h2>
+
+</div><!-- end logo -->
+
+<nav>
+	<ul>
+    <li><a class="current" href="#tut1">T<br>One</a></li>
+    <li><a class="current" href="#tut2">T<br>Two</a></li>
+     <li><a class="current" href="#tut3">T<br>Three</a></li>
+      <li><a class="current" href="#contact">Contact</a></li>
+    </ul>
+</nav>
+
+<div class="clear">
+</div><!-- end clear -->
+
+</div><!-- end inner-header -->
+
+</header>
+
+<main>
+
+<!-- HOME page -->
+
+<section class="inner-home">
+
+<div class="title-hp">
+<h1>JavaScript & jQuery</h1>
+<h2>Tutorials</h2>
+</div><!-- end title-hp -->
+
+<article class="article-hp">
+<p><span class="first-word">What</span>| JavaScript, simply put is a programming language which is used as part of the web browser. When implemented it allows client-side scripts to interact with the user, control the browser and alter teh document content that is displayed. jQuery is a free open source JavaScript library that basically makes JavaScript programming easier.</p>
+<p><span class="first-word">Why</span>| Want to design exciting and interactive web designs, learning and using JavaScript and Jquery will help you along the way to designing that "wow" type of website.</p>
+<p><span class="first-word">How</span>| Learn some basics with these three easy to master JavaScript and jquery tutorials. You will be an expert in no time.</p>
+<p><span class="first-word">Before starting</span>| To ensure all these new exciting and interactive web designs work we have a little bit of house keeping to do.</p>
+
+<ol>
+    <li>We need to download the basic jQuery link found at <a href"https://developers.google.com/speed/libraries/devguide" target="_blank">Google Developers.</a> Please click on the jQuery link which will take you to the snippet which you will need to copy and paste into the bottom of your HTML page just above the body tag. This is bascially a link to ajQuery file that is hoste by a Content Delivery Network)</li> 
+     <li>Another option is to download the jQuery source code and store it in a file locally and then link to that file in your HTML file. You can download jQuery from <a href"http://jquery.com/" target="_blank">here.</a></li>
+    </ol>
+
+</article><!-- end article-hp" -->
+</section><!-- end inner-home -->
+
+<div class="back">
+<a href="#back-top">
+</a>
+</div><!-- end back-top -->
+</main>
+
+<!-- Tutorial 1 -->
+
+<main>
+	<span class="anchor" id="tut1"></span>
+<header>
+<div class="inner-header">
+<div class="tut-logo">
+
+</div><!-- end tut-logo -->
+
+<div class="tut-title">
+<h1>Image Sliders</h1>
+<h2>It is all about the images</h2>
+
+</div><!-- end tut-title -->
+
+</div><!-- end inner-header -->
+</header>
+
+<div class="inner-tut1">
+
+<section class="tut-intro">
+<p> Everyone loves images - pretty pictures, graphics, product photos! Images help with engagement, reeling the customer stick araound a little longer. And what better way to showcase your images than with a fancy image slider/carousel. As they say an image worth a thousand words.</p>
+
+<p>The great news is nothing can be easier. Forget all that fancy JavaScript and jquery code we can pop in a creative and great looking slider with a few simple steps.</p>
+
+</section><!-- end tut-intro -->
+
+<section class="slider-demo">
+
+</section><!-- end slider-demo -->
+
+<section class="tut-steps">
+
+</section><!-- end tut-steps -->
+
+
+<section class="code-examples">
+
+<article class="html">
+
+</article><!-- end html -->
+
+
+<article calss="css">
+
+</article><!-- end css -->
+
+
+<article class="jquery">
+
+</article><!-- end jquery -->
+
+</section><!-- end code-examples -->
+
+</section><!-- end tut1-section -->
+</div><!-- end inner-tut1 -->
+</main>
+
+<div class="back">
+<a href="#back-top">
+</a>
+</div><!-- end back-top -->
+
+<!-- Tutorial 2 -->
+
+<main>
+<span class="anchor" id="tut2"></span>
+<header>
+<div class="inner-header">
+<div class="tut-logo">
+
+</div><!-- end tut-logo -->
+
+<div class="tut-title">
+<h1>Sticky Navigation Bars</h1>
+<h2>It is all about the Nav Bar</h2>
+
+</div><!-- end tut-title -->
+
+</div><!-- end inner-header -->
+</header>
+
+<div class="inner-tut2">
+
+<section class="tut-intro">
+<p> Everyone likes to see their options! That is why sticky nav bars are becoming popular on many websites especially one page sites. A fixed navigation bar remains visible at all times and with jQuery once you start scrolling down the page and the top of window hits the nav bar you can make it stick to the top and stay visible. This way the reader can always see where they are and where they want to go next without having to scroll up and down.</p>
+
+<p>Lets find out how it all works behind the scenes.</p>
+
+</section><!-- end tut-intro -->
+
+<section class="sticky-demo">
+
+</section><!-- end sticky-demo -->
+
+<section class="tut-steps">
+
+</section><!-- end tut-steps -->
+
+
+<section class="code-examples">
+
+<article class="html">
+
+</article><!-- end html -->
+
+
+<article calss="css">
+
+</article><!-- end css -->
+
+
+<article class="jquery">
+
+</article><!-- end jquery -->
+
+</section><!-- end code-examples -->
+
+</section><!-- end tut1-section -->
+</div><!-- end inner-tut2 -->
+
+<div class="back">
+<a href="#back-top">
+</a>
+</div><!-- end back-top -->
+
+</main>
+
+<!-- Tutorial 3 -->
+
+<main>
+<span class="anchor" id="tut3"></span>
+<header>
+<div class="inner-header">
+<div class="tut-logo">
+
+</div><!-- end tut-logo -->
+
+<div class="tut-title">
+<h1>Parallax Scrolling</h1>
+<h2>It is all about the Scroll</h2>
+
+</div><!-- end tut-title -->
+
+</div><!-- end inner-header -->
+</header>
+
+<div class="inner-tut3">
+
+<section class="tut-intro">
+<p>It seems people enjoy to scroll, hence the popularity of one page websites these days or readers just like information concenrtared in certain areas reducing browsing time. As they say time is of the essence. What ever the reason one page websites are popular! And hence to add greater effect to one pagers parallax scrolling websites are a hot techinque at the moment.</p>
+
+<p>So lets see how it goes down.</p>
+
+</section><!-- end tut-intro -->
+
+<section class="parallax-demo">
+
+</section><!-- end sticky-demo -->
+
+<section class="tut-steps">
+
+</section><!-- end tut-steps -->
+
+
+<section class="code-examples">
+
+<article class="html">
+
+</article><!-- end html -->
+
+
+<article calss="css">
+
+</article><!-- end css -->
+
+
+<article class="jquery">
+
+</article><!-- end jquery -->
+
+</section><!-- end code-examples -->
+
+</section><!-- end tut1-section -->
+</div><!-- end inner-tut3 -->
+
+<div class="back">
+<a href="#back-top">
+</a>
+</div><!-- end back-top -->
+
+</main>
+
+<!-- Contact -->
+
+<main>
+<span class="anchor" id="contact"></span>
+<header>
+<div class="inner-header">
+<div class="tut-logo">
+
+</div><!-- end tut-logo -->
+
+<div class="tut-title">
+<h1>Contact</h1>
+<h2>It is all about your Contacts</h2>
+
+</div><!-- end tut-title -->
+
+</div><!-- end inner-header -->
+</header>
+
+<div class="inner-contact">
+
+<section class="tut-intro">
+<p>Send me a message I am always happy to chat</p>
+
+</section><!-- end tut-intro -->
+
+<section class="form">
+
+</section><!-- end form -->
+
+</div><!-- end inner-contact -->
+
+<div class="back">
+<a href="#back-top">
+</a>
+</div><!-- end back-top -->
+
+</main>
+
+<footer>
+<div class="inner-footer">
+<p>For Educational Purposes Only</p>
+<p>&copy; Jo Demmler</p>
+<a href="http://en.wikipedia.org/wiki/JavaScript" target_"blank">Copy Credit</a> 
+<a href="#">Contact</a>
+
+<div class="back">
+<a href="#back-top">
+</a>
+</div><!-- end back-top -->
+
+</div><!--"end inner-footer" -->
+</footer>
+
+
+
+
+<script src="scripts/scripts.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</body>
+</html>
